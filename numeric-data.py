b957159ab8692e5bc4a6dba73db70a923e69f954
hannad83 <91906809+hannad83@users.noreply.github.com>
2021-12-08 15:01:22 +0000

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,13 @@
+print("Python has three numeric types: int, float, and complex")
+myvalue=1 
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+myvalue=3.14 
+print(str(myvalue) + "is of of the data type" + str(type(myvalue)))
+myvalue=5j 
+print(str(myvalue) + "is of of the data type" + str(type(myvalue)))
+myvalue=1 
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+myvalue=True 
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+myvalue=False 
+print(str(myvalue) + "is out of the data type" + str(type(myvalue)))
